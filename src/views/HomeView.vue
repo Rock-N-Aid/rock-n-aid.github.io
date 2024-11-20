5c8d2b70a467a1ca65af8879bad3e6d7cd88b1ab
Errorowy <turbokamil5@gmail.com>
2024-11-20 22:11:48 +0100

    Content Edit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -216,34 +216,54 @@
         <section class="page-section bg-light" id="team">
             <div class="container">
                 <div class="text-center">
-                    <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <h2 class="section-heading text-uppercase"><Translate pl="NASZ ZESPÓŁ" en="OUR TEAM"/></h2>
+                    <h3 class="section-subheading text-muted"><Translate pl="Opis" en="Description"/></h3>
                 </div>
                 <div class="row">
-                    <div class="col-lg-4">
+                    <div class="col" style="flex: 2 1 20%;">
                         <div class="team-member">
                             <img class="mx-auto rounded-circle" src="/assets/img/team/1.jpg" alt="..." />
-                            <h4>Parveen Anand</h4>
+                            <h4>Ziomek 1</h4>
                             <p class="text-muted">Lead Designer</p>
                             <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand Twitter Profile"><i class="fab fa-twitter"></i></a>
                             <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand Facebook Profile"><i class="fab fa-facebook-f"></i></a>
                             <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>
                         </div>
                     </div>
-                    <div class="col-lg-4">
+                    <div class="col" style="flex: 2 1 20%;">
                         <div class="team-member">
                             <img class="mx-auto rounded-circle" src="/assets/img/team/2.jpg" alt="..." />
-                            <h4>Diana Petersen</h4>
+                            <h4>Ziomek 2</h4>
                             <p class="text-muted">Lead Marketer</p>
                             <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Diana Petersen Twitter Profile"><i class="fab fa-twitter"></i></a>
                             <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Diana Petersen Facebook Profile"><i class="fab fa-facebook-f"></i></a>
                             <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Diana Petersen LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>
                         </div>
                     </div>
-                    <div class="col-lg-4">
+                    <div class="col" style="flex: 2 1 20%;">
                         <div class="team-member">
                             <img class="mx-auto rounded-circle" src="/assets/img/team/3.jpg" alt="..." />
-                            <h4>Larry Parker</h4>
+                            <h4>Ziomek 3</h4>
+                            <p class="text-muted">Lead Developer</p>
+                            <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker Twitter Profile"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker Facebook Profile"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <div class="col" style="flex: 2 1 20%;">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="/assets/img/team/3.jpg" alt="..." />
+                            <h4>Ziomek 4</h4>
+                            <p class="text-muted">Lead Developer</p>
+                            <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker Twitter Profile"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker Facebook Profile"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <div class="col" style="flex: 2 1 20%;">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="/assets/img/team/3.jpg" alt="..." />
+                            <h4>Ziomek 5</h4>
                             <p class="text-muted">Lead Developer</p>
                             <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker Twitter Profile"><i class="fab fa-twitter"></i></a>
                             <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker Facebook Profile"><i class="fab fa-facebook-f"></i></a>
@@ -252,7 +272,7 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div>
+                    <div class="col-lg-8 mx-auto text-center"><p class="large text-muted"><Translate pl="W szablonie tu był jakiś opis ale w sumie nie ma co tu napisać" en="Description"/></p></div>
                 </div>
             </div>
         </section>
@@ -279,8 +299,8 @@
         <section class="page-section" id="contact">
             <div class="container">
                 <div class="text-center">
-                    <h2 class="section-heading text-uppercase">Contact Us</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <h2 class="section-heading text-uppercase"><Translate pl="SKONTAKTUJ SIĘ Z NAMI" en="CONTACT US"/></h2>
+                    <h3 class="section-subheading text-muted"><Translate pl="Wypełnij formularz a odpowiemy tak szybko jak to tylko możliwe" en="Angielski tekst"/></h3>
                 </div>
                 <!-- * * * * * * * * * * * * * * *-->
                 <!-- * * SB Forms Contact Form * *-->
