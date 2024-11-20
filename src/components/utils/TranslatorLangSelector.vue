70d7e5f96ddc2721494486835c2d546bae3e1a8d
Errorowy <turbokamil5@gmail.com>
2024-11-20 21:51:06 +0100

    Custom Assets

--- src/components/utils/TranslatorLangSelector.vue
+++ src/components/utils/TranslatorLangSelector.vue
@@ -19,14 +19,14 @@ function langClicked(lang) {
 
 
 <template>
-              <div id = "languageSelector">
-                  <span id = "lang-0" @click="langClicked(0)" :style="{ borderWidth: store.lang == 0 ? '3px' : '0px' }">
-                      <i class="fi fi-gb"></i>
-                  </span>
-                  <span id = "lang-1" @click="langClicked(1)" :style="{ borderWidth: store.lang == 1 ? '3px' : '0px' }">
-                      <i class="fi fi-pl"></i>
-                  </span>                                        
-              </div>
+  <div id = "languageSelector">
+    <span id = "lang-1" @click="langClicked(1)" :style="{ borderWidth: store.lang == 1 ? '3px' : '0px' }">
+          <i class="fi fi-pl"></i>
+    </span>    
+    <span id = "lang-0" @click="langClicked(0)" :style="{ borderWidth: store.lang == 0 ? '3px' : '0px' }">
+          <i class="fi fi-gb"></i>
+      </span>                                  
+  </div>
 </template>
 
 <style>
