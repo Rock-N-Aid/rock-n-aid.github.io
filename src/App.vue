a7de2b805f8f0b72dd3769f7bbb1d85eb2162c53
Paprykarz-Lua <pslawinskiiii@gmail.com>
2024-11-22 20:57:58 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,3 +6,21 @@ import './assets/flag-icons-master/flag-icons.min.css'
 <template>
   <RouterView />
 </template>
+
+<script>
+
+export default {
+  created() {
+    let hash = window.location.hash;
+
+    // Check if the hash is not empty
+    if (hash && hash.substring(2) != "") {
+      // Remove the '#' character to get just the ID
+      let id = hash.substring(2);
+      window.location = window.location.toString().replace(hash, "");
+      
+    }
+  }
+}
+
+</script>
