52ef12352a72ce310bb442e5a999a3be6911f9cf
Paprykarz-Lua <120165838+Paprykarz-Lua@users.noreply.github.com>
2024-12-26 17:19:56 +0100

    logo link fix

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -7,7 +7,7 @@
 <template>
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
             <div class="container">
-                <RouterLink class="navbar-brand" to="#page-top"><img src="/assets/img/rocknaid/tekst.png" style="height: 70px;" alt="..." /></RouterLink>
+                <RouterLink class="navbar-brand" to="/#page-top"><img src="/assets/img/rocknaid/tekst.png" style="height: 70px;" alt="..." /></RouterLink>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars ms-1"></i>
