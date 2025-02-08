221c6d59fbc18719a8cbc47c7338e1013b3e48ff
Paprykarz-Lua <120165838+Paprykarz-Lua@users.noreply.github.com>
2025-02-08 16:41:04 +0100

    zsdfgzsdfzs

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -10,7 +10,7 @@
                 <RouterLink class="navbar-brand" to="/#page-top">
                     <!-- <img src="/assets/img/rocknaid/tekst.png" style="height: 70px;" alt="..." /> -->
                      <div style = "display: inline-block; height: 80px; padding: 0">
-                        <img src="/assets/img/new/text/rna-text-red.png" style="margin: 0; height: 105px; filter: drop-shadow(5px 5px 10px rgba(0, 0, 0, 1)); transform: translateY(-10px);" alt="..." />
+                        <img src="/assets/img/new/text/rna-text-white.png" style="margin: 0; height: 105px; filter: drop-shadow(5px 5px 10px rgba(0, 0, 0, 1)); transform: translateY(-10px);" alt="..." />
                      </div>
                     
                     
