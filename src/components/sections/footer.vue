51a3e615fdd0ee92137bc2c3e32a1e03db56e1ee
Paprykarz-Lua <120165838+Paprykarz-Lua@users.noreply.github.com>
2025-03-13 00:11:36 +0100

    igiujk

--- src/components/sections/footer.vue
+++ src/components/sections/footer.vue
@@ -8,15 +8,12 @@
     <footer class="footer py-4">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-lg-4 text-lg-start">Copyright &copy; Rock N' Aid 2025</div>
-                <span class="col-lg-7" style = "text-align: center;">
+                <div class="col-lg-3 text-lg-start">Copyright &copy; Rock N' Aid 2025</div>
+                <span class="col-lg-6" style = "text-align: center;">
                     <Links />
                 </span>
                 
-                <div class="col-lg-4 text-lg-end">
-                    <!-- <a class="link-dark text-decoration-none me-3" href="#!"><Translate pl="Opcja 1" en="Option 1"/></a> -->
-                    <!-- <a class="link-dark text-decoration-none" href="#!"><Translate pl="Opcja 2" en="Option 2"/></a> -->
-                </div>
+                <div class="col-lg-3 text-lg-end"><a  class = "link-dark me-3" href="/assets/Regulamin_wydarzenia_Rock_N_Aid_2025.pdf" target = "_blank">Regulamin wydarzenia</a></div>
             </div>
         </div>
     </footer>
